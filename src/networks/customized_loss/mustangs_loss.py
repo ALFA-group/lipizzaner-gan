ca0a19e897ea76beb145f2a919f35945ac177cec
Jamal Toutouh <toutouh@alfad1812.csail.mit.edu>
2019-11-07 16:47:11 -0500

    Refactoring Mustangs

--- src/networks/customized_loss/mustangs_loss.py
+++ src/networks/customized_loss/mustangs_loss.py
@@ -13,15 +13,12 @@ class MustangsLoss(torch.nn.Module):
         # We add a new parameter to network named 'randomized'. If it is set to always it selects randomly a new loss
         # every minibatch in other case it selects randomly the loss function when the network is created
         cc = ConfigurationContainer.instance()
-        if 'randomized' in cc.settings['network']:
-            self.option_randomized = 'always' in cc.settings['network']['randomized']
-            self.mustangs_always_random = self.option_randomized
-        else:
-            self.mustangs_always_random = False
+        self.mustangs_always_random_init_value = 'always' == cc.settings['network'].get('randomized', 'False')
+        self.mustangs_always_random = self.mustangs_always_random_init_value
 
         self.applied_loss = applied_loss
         if self.applied_loss is None:
-            self.pick_random_lost()
+            self.pick_random_loss()
         else:
             self.set_applied_loss(applied_loss)
 
@@ -30,10 +27,10 @@ class MustangsLoss(torch.nn.Module):
     def forward(self, input, target):
         # If we want to pick a random loss function for every minibatch
         if 'Generator' in self.name and self.mustangs_always_random:
-            self.pick_random_lost()
+            self.pick_random_loss()
         return self.applied_loss(input, target)
 
-    def pick_random_lost(self):
+    def pick_random_loss(self):
         self.applied_loss = random.choice(self.losses_list)
 
     def get_applied_loss_name(self):
@@ -48,4 +45,4 @@ class MustangsLoss(torch.nn.Module):
     def set_network_name(self, name):
         self.name = name
         init = self.mustangs_always_random
-        self.mustangs_always_random = False if ('Discriminator' in name) else self.option_randomized
+        self.mustangs_always_random = False if ('Discriminator' in name) else self.mustangs_always_random_init_value
