00f24fcc37e121c7bbe110de580ef929945a6350
Subhash Nalluru <nsn116@imperial.ac.uk>
2020-02-11 15:18:09 -0500

    Add functionality to make adv and label prediction layers cuda if cuda is enabled in SSGANs

--- src/networks/competetive_net.py
+++ src/networks/competetive_net.py
@@ -247,8 +247,8 @@ class SSDiscriminatorNet(DiscriminatorNet):
                  adv_layer, label_pred_layer, optimize_bias=True, conv=False):
         DiscriminatorNet.__init__(self, adv_loss, net, data_size, optimize_bias=optimize_bias)
         self.num_classes = num_classes
-        self.adv_layer = adv_layer
-        self.label_pred_layer = label_pred_layer
+        self.adv_layer = adv_layer.cuda() if is_cuda_enabled() else adv_layer
+        self.label_pred_layer = label_pred_layer.cuda() if is_cuda_enabled() else label_pred_layer
         self.label_pred_loss = label_pred_loss
         self.conv = conv
 
@@ -347,4 +347,25 @@ class SSGeneratorNet(GeneratorNet):
         outputs = opponent.adv_layer(opponent.net(fake_images)).view(-1)
 
         # Compute BCELoss using D(G(z))
+        return self.loss_function(outputs, real_labels), fake_images, None
+
+class GeneratorNet(CompetetiveNet):
+    @property
+    def name(self):
+        return 'Generator'
+
+    @property
+    def default_fitness(self):
+        return float('-inf')
+
+    def compute_loss_against(self, opponent, input, labels=None):
+        batch_size = input.size(0)
+
+        real_labels = to_pytorch_variable(torch.ones(batch_size))
+
+        z = noise(batch_size, self.data_size)
+
+        fake_images = self.net(z)
+        outputs = opponent.net(fake_images).view(-1)
+
         return self.loss_function(outputs, real_labels), fake_images, None
