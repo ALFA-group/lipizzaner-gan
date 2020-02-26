9c25e648be51c9d52f518b5f76458ab735ac34e4
Subhash Nalluru <nsn116@imperial.ac.uk>
2020-02-26 18:00:43 -0500

    Change GAN architecture to use SGAN architecture

--- src/networks/network_factory.py
+++ src/networks/network_factory.py
@@ -144,18 +144,29 @@ class FourLayerPerceptronFactory(NetworkFactory):
 
     @property
     def gen_input_size(self):
-        return 64
+        # return 64
+        return 128
 
     def create_generator(self, parameters=None, encoded_parameters=None):
+
+        # net = GeneratorNet(
+        #     self.loss_function,
+        #     Sequential(
+        #         nn.Linear(64, 256),
+        #         nn.LeakyReLU(0.2),
+        #         nn.Linear(256, 256),
+        #         nn.LeakyReLU(0.2),
+        #         nn.Linear(256, self.input_data_size),
+        #         nn.Tanh()), self.gen_input_size)
 
         net = GeneratorNet(
             self.loss_function,
             Sequential(
-                nn.Linear(64, 256),
+                nn.Linear(128, 256),
                 nn.LeakyReLU(0.2),
-                nn.Linear(256, 256),
+                nn.Linear(256, 512),
                 nn.LeakyReLU(0.2),
-                nn.Linear(256, self.input_data_size),
+                nn.Linear(512, self.input_data_size),
                 nn.Tanh()), self.gen_input_size)
 
         if parameters is not None:
@@ -167,6 +178,16 @@ class FourLayerPerceptronFactory(NetworkFactory):
 
     def create_discriminator(self, parameters=None, encoded_parameters=None):
 
+        # net = DiscriminatorNet(
+        #     self.loss_function,
+        #     Sequential(
+        #         nn.Linear(self.input_data_size, 256),
+        #         nn.LeakyReLU(0.2),
+        #         nn.Linear(256, 256),
+        #         nn.LeakyReLU(0.2),
+        #         nn.Linear(256, 1),
+        #         nn.Sigmoid()), self.gen_input_size)
+
         net = DiscriminatorNet(
             self.loss_function,
             Sequential(
@@ -174,7 +195,9 @@ class FourLayerPerceptronFactory(NetworkFactory):
                 nn.LeakyReLU(0.2),
                 nn.Linear(256, 256),
                 nn.LeakyReLU(0.2),
-                nn.Linear(256, 1),
+                nn.Linear(256, 512),
+                nn.LeakyReLU(0.2),
+                nn.Linear(512, 1),
                 nn.Sigmoid()), self.gen_input_size)
 
         if parameters is not None:
