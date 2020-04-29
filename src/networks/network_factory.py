Fix batch normalization error

src/networks/network_factory.py
@@ -597,7 +597,7 @@ class SSGANConvMNIST28x28NetworkFactory(NetworkFactory):
             # Sequential(nn.Conv2d(self.complexity * 8, self.num_classes + 1, 4, 1, 0)),
             Sequential(
                 nn.Linear(784, 4096),
-                nn.BatchNorm1d(self.complexity * 2),
+                nn.BatchNorm1d(4096),
                 nn.LeakyReLU(0.2, inplace=True),
                 View((1, 64, 64)),
                 nn.Conv2d(1, self.complexity, 4, 2, 1),