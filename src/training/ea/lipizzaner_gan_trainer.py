47dca3b9682e0e698aade9d561ca3a9d653cdd8f
Subhash Nalluru <nsn116@imperial.ac.uk>
2020-05-09 02:59:10 +0530

    Fix error when using majority voting for discriminator testing

--- src/training/ea/lipizzaner_gan_trainer.py
+++ src/training/ea/lipizzaner_gan_trainer.py
@@ -323,8 +323,8 @@ class LipizzanerGANTrainer(EvolutionaryAlgorithmTrainer):
         with torch.no_grad():
             for data, target in test_loader:
                 data, target = data.to(device), target.to(device)
-                # data = data.view(-1, 784)
-                data = data.view(-1, 1, 28, 28)
+                data = data.view(-1, 784)
+                # data = data.view(-1, 1, 28, 28)
                 pred_accumulator = []
                 for model in models:
                     output = model.classification_layer(model.net(data))
@@ -332,7 +332,8 @@ class LipizzanerGANTrainer(EvolutionaryAlgorithmTrainer):
                     pred = output.argmax(dim=1, keepdim=True)
                     pred_accumulator.append(pred.view(-1))
                 label_votes = torch.tensor(list(zip(*pred_accumulator)))
-                prediction = torch.tensor([labels.bincount(minlength=10).argmax() for labels in label_votes])
+                prediction = torch.tensor([labels.bincount(minlength=11).argmax() for labels in label_votes])
+                print(prediction.shape)
                 correct += prediction.eq(target.view_as(prediction)).sum().item()
 
         num_samples = len(test_loader.dataset)
@@ -346,8 +347,8 @@ class LipizzanerGANTrainer(EvolutionaryAlgorithmTrainer):
         with torch.no_grad():
             for data, target in test_loader:
                 data, target = data.to(device), target.to(device)
-                # data = data.view(-1, 784)
-                data = data.view(-1, 1, 28, 28)
+                data = data.view(-1, 784)
+                # data = data.view(-1, 1, 28, 28)
                 output = model.classification_layer(model.net(data))
                 output = output.view(-1, 11)
                 pred = output.argmax(dim=1, keepdim=True)
