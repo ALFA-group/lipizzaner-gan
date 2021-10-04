1a12dcae0eecbe334c4bc876b856bf5e11bb68fe
andresvidalberriel <andresvidalberriel@gmail.com>
2021-10-04 00:44:26 -0300

    Remove innecesary helpers

--- src/helpers/reproducible_helpers.py
+++ src/helpers/reproducible_helpers.py
@@ -4,10 +4,6 @@ import random
 import numpy as np
 import torch
 
-MAXINT = 2 ** 32 - 1
-
-MAXINT = 2 ** 32 - 1
-
 
 def set_random_seed(seed, cuda):
     random.seed(seed)
@@ -18,14 +14,3 @@ def set_random_seed(seed, cuda):
         torch.backends.cudnn.deterministic = True
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-
-
-def get_heuristic_seed(seed, ip, port):
-    """
-    Heuristic method to obtain seed number based on client IP and port
-    (Since it is desired to have different seed for different clients to ensure diversity)
-    """
-    # TODO Handle the case of integer overflow
-    seed + int(ip.replace(".", "")) + 1000 * port
-    seed = seed % MAXINT
-    return seed
